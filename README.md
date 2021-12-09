6baeaf7f28e7c5c15fd3549d22a01ec9a1262555
Daniel Castro <dcmusic01@gmail.com>
2021-12-08 22:55:27 -0800

    added read me notes

--- README.md
+++ README.md
@@ -13,5 +13,6 @@ I build an app with a direct and powerful purpose. It does all of the things tha
 
 ### Reflections and Comments
 
-* Consider including the answers to your daily journal and submission questions here
-* This is also a good place to reflect on the tools and resources used and learned
+* I find that it's harder for me somwtimes to ask for help but not because I'm scared to ask, but because I feel I can figure out the problem. Funny enough this is exactly what they said not to do.  15 min ask questions. 
+* Learning about the way I learn is an intersting concept but an imprtant process.  I realized that reading ahead for me has played a better role in this class.  I'm able to at least be somewhat familiarized with the material. 
+* Repetition is what is helping me learn as well. I am ery hands on type of learner.  The reading portions I don't seem to gain much ground in knowldge but serve its purpose.
--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ let doILikeMusic;
 function musicQuestion() {
   doILikeMusic = prompt('Do I like music? Yes or No?');
   doILikeMusic.toLowerCase();
+  doILikeMusic.toUpperCase();
   console.log(doILikeMusic);
 
   if (doILikeMusic === 'yes') {
@@ -26,6 +27,7 @@ let instrument;
 function instrumentQuestion() {
   instrument = prompt('what instrument do I play? Piano or Drums?');
   instrument.toLowerCase();
+  instrument.toUpperCase();
   console.log(instrument);
 
   if (instrument === 'piano') {
@@ -38,7 +40,77 @@ function instrumentQuestion() {
   }
 }
 
+let travel;
+
+function travelQuestion() {
+  travel = prompt('Do I like travelling? Yes or No?');
+  travel.toLowerCase();
+  travel.toUpperCase();
+  console.log(travel);
+
+  if (travel === 'yes') {
+    //console.log('Yes, I do like to travel!')
+
+    alert('Yes, I like to travel!');
+  } else {
+    //console.log('No, I do like travelling');
+    alert('No, I do like travelling!');
+  }
+}
+let cubanFood;
+
+function foodQuestion() {
+  cubanFood = prompt('Do I like cuban food? Yes or No?');
+  cubanFood.toLowerCase();
+  cubanFood.toUpperCase();
+  console.log(cubanFood);
+
+  if (cubanFood === 'yes') {
+    //console.log('Yes, I do love cuban food!')
+
+    alert('Yes, I love cuban food!');
+  } else {
+    //console.log('No, I do like cuban food');
+    alert('No, I do like cuban food!');
+  }
+}
+
+let running;
+
+function runningQuestion() {
+  running = prompt('Do I like running? Yes or No?');
+  running.toLowerCase();
+  running.toUpperCase();
+  console.log(running);
+
+  if (running === 'no') {
+    //console.log('youre correct! I don't like running.')
+
+    alert('youre correct! I dont like running.');
+  } else {
+    //console.log('No, I do like travelling');
+    alert('No, I do like running!');
+  }
+}
+
+let game;
+
+function gameQuestion() {
+  game = prompt('Did you have fun?');
+  game.toLowerCase();
+  console.log(game);{
+
+ 
+
+    alert('Thanks for playin ' + userName + '!');
+  } 
+}
 
 
 musicQuestion();
 instrumentQuestion();
+travelQuestion();
+foodQuestion();
+runningQuestion();
+gameQuestion();
+
