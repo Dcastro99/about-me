322c3e21dbf1a39601582df4dc10bb8e2c0baf4e
Daniel Castro <dcmusic01@gmail.com>
2021-12-13 17:39:17 -0800

    added y or no option

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## About Me
 
-I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+I love the creative process in all my work. On this page I'm able to shed a little light on my life and things I like to do. Creativity is a driving force in my life and I like to leave my work feeling proud of what I've achieved during the process.
 
 ### Danny Castro
 
@@ -16,3 +16,9 @@ I build an app with a direct and powerful purpose. It does all of the things tha
 * I find that it's harder for me somwtimes to ask for help but not because I'm scared to ask, but because I feel I can figure out the problem. Funny enough this is exactly what they said not to do.  15 min ask questions. 
 * Learning about the way I learn is an intersting concept but an imprtant process.  I realized that reading ahead for me has played a better role in this class.  I'm able to at least be somewhat familiarized with the material. 
 * Repetition is what is helping me learn as well. I am ery hands on type of learner.  The reading portions I don't seem to gain much ground in knowldge but serve its purpose.
+
+* today we learned about arrays. This was difficult but fun to play with.  I was able to ask a friend to help me understand a little better.
+
+* Created a variable that counted all the answers correct and tallied them up. this took me a while as well.  I had to reverse engeneer the answer which was fun for me and realized this is a great learning tool for me.
+
+* I also copied my codes and saved them in order to see if I can replicated them  in replit for practice purposes.
--- js/app.js
+++ js/app.js
@@ -4,108 +4,155 @@
 
 let userName = prompt('whats you name?');
 alert('Hi' + ', ' + userName);
-
-
-let doILikeMusic;
+let totalAnswers = 0;
 
 function musicQuestion() {
-  doILikeMusic = prompt('Do I like music? Yes or No?').toLowerCase();
+
+  let doILikeMusic = prompt('Do I like music? Yes or No?').toLowerCase();
 
   console.log(doILikeMusic);
 
-  if (doILikeMusic === 'yes') {
+  if (doILikeMusic.slice(0,1) ==='y') {
     // console.log('Yes, I like music!'); }
     alert('Yes, I like music!');
+    totalAnswers++;
   } else {
     //console.log('No, I do like music');
     alert('No, I do like music');
   }
 }
-let instrument;
 
 function instrumentQuestion() {
-  instrument = prompt('what instrument do I play? Piano or Drums?').toLowerCase();
+  let instrument = prompt('Do I like playing the banjo? yes or no?').toLowerCase();
 
   console.log(instrument);
 
-  if (instrument === 'piano') {
+  if (instrument.slice(0,1) === 'n') {
     //console.log('Yes, I play piano!')
 
-    alert('Yes, I play piano!');
+    alert('correct, I do not like playing the banjo!');
+    totalAnswers++;
   } else {
     //console.log('No, I play piano');
-    alert('No, I play piano');
+    alert('No, I do not like playing the banjo.');
   }
 }
 
-let travel;
+
 
 function travelQuestion() {
-  travel = prompt('Do I like travelling? Yes or No?').toLowerCase();
+  let travel = prompt('Do I like travelling? Yes or No?').toLowerCase();
 
   console.log(travel);
 
-  if (travel === 'yes') {
+  if (travel.slice(0,1) === 'y') {
     //console.log('Yes, I do like to travel!')
 
     alert('Yes, I like to travel!');
+    totalAnswers++;
   } else {
     //console.log('No, I do like travelling');
     alert('No, I do like travelling!');
   }
 }
-let cubanFood;
 
 function foodQuestion() {
-  cubanFood = prompt('Do I like cuban food? Yes or No?').toLowerCase();
-
+  let cubanFood = prompt('Do I like cuban food? Yes or No?').toLowerCase();
   console.log(cubanFood);
 
-  if (cubanFood === 'yes') {
+  if (cubanFood.slice(0,1) === 'y') {
     //console.log('Yes, I do love cuban food!')
 
     alert('Yes, I love cuban food!');
+    totalAnswers++;
   } else {
     //console.log('No, I do like cuban food');
     alert('No, I do like cuban food!');
   }
 }
-
-let running;
-
 function runningQuestion() {
-  running = prompt('Do I like running? Yes or No?').toLowerCase();
+  let running = prompt('Do I like running? Yes or No?').toLowerCase();
 
   console.log(running);
 
-  if (running === 'no') {
+  if (running.slice(0,1) === 'n') {
     //console.log('youre correct! I don't like running.')
 
     alert('youre correct! I dont like running.');
+    totalAnswers++;
   } else {
     //console.log('No, I do like travelling');
-    alert('No, I do like running!');
+    alert('No, I do not like running!');
+  }
+}
+function howManyInstruments(){
+  let answer = '';
+  let tries = 0;
+  let correctAnswer = false;
+  do{
+    answer = prompt('How many instruments do I play? (1-10)?');
+    tries++;
+    if(answer > 4){
+      alert('Try again');
+      alert('You are too high!');
+    }else if(answer < 4){
+      alert('Try again');
+      alert('You are too low!');
+    } else{
+      alert('You are correct!');
+      correctAnswer = true;
+      totalAnswers++;
+    }
+  }
+
+  while(answer !=='4'&& tries<4);
+  if(!correctAnswer){
+    alert('Nice try, but I play 4 instruments!');
   }
 }
 
-let game;
+function instrumentIPlay(){
 
-function gameQuestion() {
-  game = prompt('Did you have fun?').toLowerCase();
+  let answers = ['piano','guitar','bass','drums'];
+  let attempts = 6;
+  let correctGuess = false;
 
-  console.log(game);{
+  do{
+    let answer = prompt('What instruments do I play?');
+    attempts--;
+    let found = false;
+    for (let i = 0; i < answers.length; i++){
+      if (answers[i] === answer){
+        found = true;
+      }
+    }
+    if (found){
 
- 
-
-    alert('Thanks for playin ' + userName + '!');
-  } 
+      alert('That is correct! I play '+ answer);
+      correctGuess = true;
+      totalAnswers++;
+    } else{
+      alert('Nope! Try again! You have ' + attempts + ' attepmt(s) left.');
+    }
+  }
+  while(!correctGuess && attempts!==0);
+  if(!correctGuess){
+    alert('Nice try, but I play piano, guitar, drums, and bass!');
+  }
 }
 
+function gameQuestion() {
 
+  alert('Thanks for playin ' + userName + '!');
+  alert('You got ' + totalAnswers + ' out of 7 questions correct!');
+
+}
 musicQuestion();
-instrumentQuestion();
 travelQuestion();
 foodQuestion();
 runningQuestion();
+instrumentQuestion();
+howManyInstruments();
+instrumentIPlay();
 gameQuestion();
 
