1105a4bc10f0085fdef4b40f332688be899fad61
laptopadventure <bazelart13@gmail.com>
2021-12-15 19:18:21 -0800

    converts specific question functions into genericized function for booleans

--- js/app.js
+++ js/app.js
@@ -6,113 +6,32 @@ let userName = prompt('whats you name?');
 alert('Hi' + ', ' + userName);
 let totalAnswers = 0;
 
-yesOrNoQuestion('Do I like pineapples?', 'y', 'Yes, I like pineapples', 'No, I don\'t like pineapples');
-
 function yesOrNoQuestion(question, expectedAnswer, correctResponse, incorrectResponse) {
   let userAnswer = prompt(question).toLowerCase().slice(0,1);
 
-  let incorrectAnswer
+  let incorrectAnswer;
   //parse expected answer and create incorrect answer as the opposite
 
   if(expectedAnswer === 'y') {
-    incorrectAnswer = 'n'
+    incorrectAnswer = 'n';
   } else {
-    incorrectAnswer = 'y'
+    incorrectAnswer = 'y';
   }
 
   //check answer
   if(userAnswer === expectedAnswer) {
     //yes cool
-    totalAnswers++
-    alert(correctResponse)
+    totalAnswers++;
+    alert(correctResponse);
   } else if(userAnswer === incorrectAnswer) {
     //wrong rip
-    alert(incorrectResponse)
+    alert(incorrectResponse);
   } else {
     //??? gibberish
-    alert("Yes or no, please!")
+    alert('Yes or no, please!');
   }
 }
 
-function musicQuestion() {
-
-  let doILikeMusic = prompt('Do I like music? Yes or No?')
-
-  //console.log(doILikeMusic);
-
-  if (doILikeMusic.slice(0,1) ==='y') {
-    // console.log('Yes, I like music!'); }
-    alert('Yes, I like music!');
-    totalAnswers++;
-  } else {
-    //console.log('No, I do like music');
-    alert('No, I do like music');
-  }
-}
-
-function instrumentQuestion() {
-  let instrument = prompt('Do I like playing the banjo? Yes or No?').toLowerCase();
-
-  //console.log(instrument);
-
-  if (instrument.slice(0,1) === 'n') {
-    //console.log('Yes, I play piano!')
-
-    alert('correct, I don\'t like playing the banjo!');
-    totalAnswers++;
-  } else {
-    //console.log('No, I play piano');
-    alert('No, I don\'t like playing the banjo.');
-  }
-}
-
-
-
-function travelQuestion() {
-  let travel = prompt('Do I like travelling? Yes or No?').toLowerCase();
-
-  console.log(travel);
-
-  if (travel.slice(0,1) === 'y') {
-    //console.log('Yes, I do like to travel!')
-
-    alert('Yes, I like to travel!');
-    totalAnswers++;
-  } else {
-    //console.log('No, I do like travelling');
-    alert('No, I do like travelling!');
-  }
-}
-
-function foodQuestion() {
-  let cubanFood = prompt('Do I like cuban food? Yes or No?').toLowerCase();
-  console.log(cubanFood);
-
-  if (cubanFood.slice(0,1) === 'y') {
-    //console.log('Yes, I do love cuban food!')
-
-    alert('Yes, I love cuban food!');
-    totalAnswers++;
-  } else {
-    //console.log('No, I do like cuban food');
-    alert('No, I do like cuban food!');
-  }
-}
-function runningQuestion() {
-  let running = prompt('Do I like running? Yes or No?').toLowerCase();
-
-  console.log(running);
-
-  if (running.slice(0,1) === 'n') {
-    //console.log('youre correct! I don't like running.')
-
-    alert('You\'re correct! I don\'t like running.');
-    totalAnswers++;
-  } else {
-    //console.log('No, I do like travelling');
-    alert('No, I don\'t like running!');
-  }
-}
 function howManyInstruments(){
   let answer = '';
   let tries = 0;
@@ -175,12 +94,11 @@ function gameQuestion() {
   alert('You got ' + totalAnswers + ' out of 7 questions correct!');
 
 }
-musicQuestion();
-travelQuestion();
-foodQuestion();
-runningQuestion();
-instrumentQuestion();
+yesOrNoQuestion('Do I like music? Yes or No?', 'y', 'Yes, I like music!', 'No, I do like music');
+yesOrNoQuestion('Do I like travelling? Yes or No?', 'y', 'Yes, I like to travel!', 'No, I do like travelling!');
+yesOrNoQuestion('Do I like cuban food? Yes or No?', 'y', 'Yes, I love cuban food!', 'No, I do like cuban food!');
+yesOrNoQuestion('Do I like running? Yes or No?', 'n', 'You\'re correct! I don\'t like running.', 'No, I don\'t like running!');
+yesOrNoQuestion('Do I like playing the banjo? Yes or No?', 'n', 'Correct, I don\'t like playing the banjo!', 'No, I don\'t like playing the banjo.');
 howManyInstruments();
 instrumentIPlay();
 gameQuestion();
-
